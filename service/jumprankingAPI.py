59260244c54777b69d3bedce20ff42f0fe3efea4
rdecelis <rdecelis@stt-systems.com>
2019-06-17 18:03:12 +0200

    Add Record class for DB

--- service/jumprankingAPI.py
+++ service/jumprankingAPI.py
@@ -5,6 +5,24 @@ import linkero.core.linkero as linkero
 
 api_base_path = "/jumpranking/api/v1"
 
+# extensions
+#db = SQLAlchemy(app)
+#linkero.db
+
+
+class Record(linkero.db.Model):
+    __tablename__ = "records"
+    id = linkero.db.Column(linkero.db.Integer, primary_key=True)
+    user = linkero.db.Column(linkero.db.String(45))
+    height = linkero.db.Column(linkero.db.Float)
+
+    def __init__(self, user, height):
+        self.user = user
+        self.height = height
+
+    def __repr__(self):
+        return '<Record %r - %r -%r -%r>' % (self.id, self.user, self.height)
+
 
 class Record(linkero.Resource):
     def get(self, id):
