f18a801e7919f6ea24a5f19f258e560c205dc3d3
Rubén de Celis Hernández <contact@rdch106.hol.es>
2019-06-18 00:04:11 +0200

    Add to_json static method in Record class

--- service/jumprankingAPI.py
+++ service/jumprankingAPI.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import linkero.core.linkero as linkero
+from collections import Iterable
 
 
 api_base_path = "/jumpranking/api/v1"
@@ -8,6 +9,7 @@ api_base_path = "/jumpranking/api/v1"
 # extensions
 #db = SQLAlchemy(app)
 #linkero.db
+record_prototype = {'id': None, 'user': None, 'height': None}
 
 
 class Record(linkero.db.Model):
@@ -22,6 +24,18 @@ class Record(linkero.db.Model):
 
     def __repr__(self):
         return '<Record %r - %r - %r>' % (self.id, self.user, self.height)
+
+    @staticmethod
+    def to_json(result):
+        data = {}
+        records = []
+        if isinstance(result, Iterable):
+            for record in result:
+                records.append(dict(record_prototype, id=record.id, user=record.user, height=record.height))
+        else:
+            records.append(dict(record_prototype, id=result.id, user=result.user, height=result.height))
+        data['data'] = records
+        return data
 
 
 class Register(linkero.Resource):
