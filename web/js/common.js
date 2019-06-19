d6b52f4adfc884788499921e90276c3e62ad2446
rdecelis <rdecelis@stt-systems.com>
2019-06-19 12:55:40 +0200

    Add common.js to web

---
+++ web/js/common.js
@@ -0,0 +1,35 @@
+
+var ranking;
+var maxranking = 100;  // Max ranking elements (TOP 100)
+    
+function printRanking(ranking){
+    ranking.forEach(function(arrayItem){            
+            $("#ranking tbody").append("<tr><td>"+arrayItem.user
+            +"</td><td>"+arrayItem.height
+            +"</td></tr>");
+        });
+        
+}
+    
+function getRanking(region){
+    
+    $.ajax({type:"GET", url: "http://192.168.1.224:5000/jumpranking/api/v1/registers", dataType: "json", async: true,
+        success: function(data) {            
+            printRanking(data.data.slice(0, maxranking));
+            ranking =$("#ranking").DataTable( 
+            {
+                "order": [[ 1, "desc" ]],
+                "responsive": true
+            } );
+        }
+    });
+}
+
+function init(region){
+    
+    $(document).ready(function () {
+         getRanking(region);
+         $("#selection").val(region);    
+    });
+    
+}
