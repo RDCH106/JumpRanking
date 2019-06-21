4c7ca434acf661e4a2b1985c1822771b89489851
rdecelis <rdecelis@stt-systems.com>
2019-06-21 13:04:53 +0200

    Update api_url

--- web/js/common.js
+++ web/js/common.js
@@ -1,7 +1,7 @@
 
 var ranking;
 var maxranking = 100;  // Max ranking elements (TOP 100)
-var api_url = "http://192.168.1.224:5000";
+var api_url = "http://jump-api.stt-systems.com:5030";
 var update_flag;
 var update_interval = 30000;
 var datatable_options = {
