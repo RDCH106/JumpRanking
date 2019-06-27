6d2ea162c72d1ed0bfc08fa3cd3f795fadbe72f5
Rubén de Celis Hernández <contact@rdch106.hol.es>
2019-06-28 00:58:51 +0200

    Update GUI elements

--- sender/jumpranking-sender.py
+++ sender/jumpranking-sender.py
@@ -20,18 +20,22 @@ class App:
         self.user_i.pack(side=LEFT, padx=5, pady=20)
 
         self.spacer = Label(master, text="")
-        self.spacer.pack(side=LEFT, padx=10, pady=20)
+        self.spacer.pack(side=LEFT, padx=5, pady=20)
 
         self.height_l = Label(master, text="Height:")
         self.height_l.pack(side=LEFT, padx=5, pady=20)
         self.height_i = Entry(master)
         self.height_i.pack(side=LEFT, padx=5, pady=20)
 
-        self.response_l = Label(master, text="")
+        self.spacer = Label(master, text="")
+        self.spacer.pack(side=RIGHT, padx=5, pady=20)
+
+        self.quit_b = Button(master, text="SEND", command=self.send_request)
+        self.quit_b.pack(side=RIGHT, padx=5, pady=20)
+
+        self.response_l = Label(master, text=" ")
         self.response_l.pack(side=RIGHT, padx=5, pady=20)
 
-        self.quit_b = Button(master, text="SEND", command=sys.exit)
-        self.quit_b.pack(side=RIGHT, padx=20, pady=20)
 
 
 root = Tk()
