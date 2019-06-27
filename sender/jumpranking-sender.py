81e8d29120818f7de679593e866adb3e11f46762
Rubén de Celis Hernández <contact@rdch106.hol.es>
2019-06-28 00:10:53 +0200

    Add minimal GUI to sender

--- sender/jumpranking-sender.py
+++ sender/jumpranking-sender.py
@@ -10,8 +10,21 @@ class App:
         master.title("Jump Ranking Sender")
         frame.pack()
 
-        self.quit_b = Button(master, text="Quit", command=sys.exit)
-        self.quit_b.pack()
+        self.user_l = Label(master, text="User:")
+        self.user_l.pack(side=LEFT, padx=5, pady=20)
+        self.user_i = Entry(master)
+        self.user_i.pack(side=LEFT, padx=5, pady=20)
+
+        self.spacer = Label(master, text="")
+        self.spacer.pack(side=LEFT, padx=10, pady=20)
+
+        self.height_l = Label(master, text="Height:")
+        self.height_l.pack(side=LEFT, padx=5, pady=20)
+        self.height_i = Entry(master)
+        self.height_i.pack(side=LEFT, padx=5, pady=20)
+
+        self.quit_b = Button(master, text="SEND", command=sys.exit)
+        self.quit_b.pack(side=RIGHT, padx=20, pady=20)
 
 
 root = Tk()
